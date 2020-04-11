432afbbde7f6c61ddf9a76c6c1be4a812be26aa4
Michal Mrotek <michal.mrotek@300brains.com>
2020-04-11 21:00:01 +0200

    [1] context path

--- build/VisGraph.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-declare type Props = {
-    styles?: React.CSSProperties;
-    id?: string;
-    className?: string;
-};
-export declare const VisGraph: FC<Props>;
-export {};
--- build/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { useVis } from "./useVis";
-import { VisGraph } from "./VisGraph";
-import { VisProvider } from "./VisProvider";
-export default VisProvider;
-export { VisGraph, useVis, VisProvider };
